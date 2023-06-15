00f8e3acee30a8025ab10603fcc55f967055bf30
Veronika Pomyateeva <pomyateevav@gmail.com>
2023-06-14 22:07:45 -0400

    added time complexity explanation to the algorithm to sum digtis in a number

--- practice/22-sumDigitsInNumber.js
+++ practice/22-sumDigitsInNumber.js
@@ -19,3 +19,9 @@ sumDigits(testNumber2); // should console log 1
 
 // Big O Time Complexity Explanation 
 
+// The runtime is the number of digits in the number
+
+// A number with digits d can have a value of up to 10^d
+// Then d = log n
+
+// Runtime complexity is O(log n)
