14e1172a4b527b1234fda167d99033d537e92245
Veronika Pomyateeva <pomyateevav@gmail.com>
2023-06-14 21:59:21 -0400

    fixed a bun in not counting 1 in the algorithm to add digits of a number together

--- practice/22-sumDigitsInNumber.js
+++ practice/22-sumDigitsInNumber.js
@@ -1,10 +1,11 @@
 const testNumber = 4444;
+const testNumber2 = 1000;
 
 function sumDigits (input) {
 
     let sumOfDigits = 0;
 
-    while (input > 1) {
+    while (input >= 1) {
         sumOfDigits += input % 10;
         input = Math.floor(input / 10);
     };
@@ -13,6 +14,8 @@ function sumDigits (input) {
 };
 
 sumDigits(testNumber); // should console log 16
+
+sumDigits(testNumber2); // should console log 1
 
 // Big O Time Complexity Explanation 
 
